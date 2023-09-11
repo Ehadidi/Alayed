830ce3a2897decc1fbee354020ea2b142f5f2548
salah elhadidi <sala7el7adidi@gmail.com>
2023-09-11 11:30:51 +0300

    sheet test2

--- src/components/about/legalAffairs.vue
+++ src/components/about/legalAffairs.vue
@@ -2,15 +2,15 @@
   <section class="P_top_60 P_bottom_60">
     <div class="d-flex align-items-center justify-content-center gap20 flex-column">
         <h4 class="mainColor fontBold mb-0">{{ $t('about.legalAffairs') }}</h4>
-        <span class="font12 fontBold d-flex"><span>{{ $t('about.legalAffairsNo') }}</span> <span>: 9661633366+</span></span>
-        <span class="font12 fontBold d-flex"><span>{{ $t('about.email') }}</span><span>: sample_email.com</span></span>
+        <span class="font12 fontBold d-flex"><span>{{ $t('about.legalAffairsNo') }}</span> <span>: {{ aboutData.legal_affairs_phone }}</span></span>
+        <span class="font12 fontBold d-flex"><span>{{ $t('about.email') }}</span><span>: {{ aboutData.legal_affairs_email }}</span></span>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-
+    props : ['aboutData']
 }
 </script>
 
